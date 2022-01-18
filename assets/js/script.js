adaab88e91b04d95a259380da3e5748b93c38457
webiotsanjali <anjali.patel@webiots.in>
2022-01-18 18:37:01 +0530

    ratio remove from all images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,6 @@
 (function ($) {
     "use strict";
 
-    /*=====================
-     Image to background js
-     ==========================*/
-    $(".bg-top").parent().addClass('b-top');
-    $(".bg-bottom").parent().addClass('b-bottom');
-    $(".bg-center").parent().addClass('b-center');
-    $(".bg-left").parent().addClass('b-left');
-    $(".bg-right").parent().addClass('b-right');
-    $(".bg_size_content").parent().addClass('b_size_content');
-    $(".bg-img").parent().addClass('bg-size');
-    $(".bg-img.blur-up").parent().addClass('blur-up lazyload');
-    $('.bg-img').each(function () {
-
-        var el = $(this),
-            src = el.attr('src'),
-            parent = el.parent();
-
-
-        parent.css({
-            'background-image': 'url(' + src + ')',
-            'background-size': 'cover',
-            'background-position': 'center',
-            'background-repeat': 'no-repeat',
-            'display': 'block'
-        });
-
-        el.hide();
-    });
-
     $(".navbar-toggler").on('click', function() { 
         $('.bg-overlay').addClass('active');
     });
